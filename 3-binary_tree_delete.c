a22b9e16f013785bdf80a8925cc7c0c506080a74
zeethebeast <chimasamuel6@gmail.com>
2024-06-06 17:58:27 +0100

    trees

---
+++ 3-binary_tree_delete.c
@@ -0,0 +1,27 @@
+#include "binary_trees.h"
+
+/**
+ * binary_tree_delete - deletes a tree
+ * @tree: A pointer to the root node of the tree to delete.
+ * Return: NULL.
+ */
+
+void binary_tree_delete(binary_tree_t *tree)
+{
+	if (tree == NULL)
+	{
+		return;
+	}
+
+	if (tree->left != NULL)
+	{
+		binary_tree_delete(tree->left); 
+	}
+
+	if (tree->right != NULL)
+	{
+		binary_tree_delete(tree->right);
+	}
+
+	free(tree);
+}
