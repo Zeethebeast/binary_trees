3be5a92f6dd3b4cd39943841352c2b201c6a8f43
zeethebeast <chimasamuel6@gmail.com>
2024-06-07 19:35:52 +0100

    trees

--- 7-binary_tree_inorder.c
+++ 7-binary_tree_inorder.c
@@ -4,7 +4,6 @@
  * binary_tree_inorder - trasverses a binary-tree using in-order
  * @tree: pointer to the node
  * @func: A funtion pointer
- * @Return: 0 on success and 1 on failure
  */
 
 void binary_tree_inorder(const binary_tree_t *tree, void (*func)(int))
